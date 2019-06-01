24375ebe01943e0d79a9a9ad1d8a67c63d3a38dd
wasborn11 <archibald.ff14@gmail.com>
2019-06-01 10:06:24 +0900

    unless文

---
+++ hello.rb
@@ -0,0 +1,33 @@
+# status = 'error'
+# if status != 'ok'
+#   puts '何か異常があります'
+# end
+#
+# status = 'error'
+# unless status == 'ok'
+#   puts '何か異常があります'
+# end
+
+# status = 'ok'
+# unless status == 'ok'
+#   puts '何か異常があります'
+# else
+#   puts '正常です'
+# end
+
+# status = 'error'
+
+# message =
+# unless status == 'ok'
+#   '何か異常があります'
+# else
+#   '正常です'
+# end
+#
+# puts message
+
+# puts '何か異常があります' unless status == 'ok'
+
+# unless status == 'ok' then
+#   puts '何か異常があります'
+# end
