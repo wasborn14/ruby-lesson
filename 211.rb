9b8b851d3e405c869311ce695924aa91f26832de
wasborn11 <archibald.ff14@gmail.com>
2019-06-01 17:22:36 +0900

    2章終了！

---
+++ 211.rb
@@ -0,0 +1,49 @@
+# def greeting(country)
+#   if country =='japan'
+#     'こんにちは'
+#   else
+#     'hello'
+#   end
+# end
+#
+# puts greeting
+#
+# puts greeting('us')
+#
+# puts greeting('us', 'japan')
+
+# def greeting(country = 'japan')
+#   if country =='japan'
+#     'こんにちは'
+#   else
+#     'hello'
+#   end
+# end
+#
+# puts greeting
+#
+# puts greeting('us')
+#
+# puts greeting('us', 'japan')
+
+# def default_args(a, b, c = 0, d = 0)
+#   puts "a=#{a}, b=#{b}, c=#{c}, d=#{d}"
+# end
+# default_args(1, 2)
+# default_args(1, 2, 3)
+# default_args(1, 2, 3, 4)
+
+# def foo(time = Time.now, message = bar)
+#   puts "time: #{time}, message: #{message}"
+# end
+#
+# def bar
+#   'BAR'
+# end
+#
+# puts foo
+
+# require 'date'
+# puts Date.today
+
+puts 'abc'.inspect
