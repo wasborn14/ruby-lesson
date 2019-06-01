d27239e5a0941eac0e57be4a032e712fbdceb7d5
wasborn11 <archibald.ff14@gmail.com>
2019-06-01 10:27:09 +0900

    条件演算子

--- hello.rb
+++ hello.rb
@@ -1,53 +1,9 @@
-# country = 'italy'
-#
-# if country == 'japan'
-#   puts 'こんにちは'
-# elsif country == 'us'
-#   puts 'hello'
-# elsif country == 'italy'
-#   puts 'ciao'
+n = 11
+# if n > 10
+#   puts '10より大きい'
 # else
-#   puts '???'
+#   puts '10以下'
 # end
 
-# country = 'italy'
-#
-# case country
-# when 'japan'
-#   puts 'こんにちは'
-# when 'us'
-#   puts 'hello'
-# when 'italy'
-#   puts 'ciao'
-# else
-#   puts '???'
-# end
-
-# country = 'アメリカ'
-#
-# case country
-# when 'japan', '日本'
-#   puts 'こんにちは'
-# when 'us', 'アメリカ'
-#   puts 'hello'
-# when 'italy', 'イタリア'
-#   puts 'ciao'
-# else
-#   puts '???'
-# end
-
-country = 'italy'
-
-message =
-case country
-when 'japan'
-  'こんにちは'
-when 'us'
-  'hello'
-when 'italy'
-  'ciao'
-else
-  '???'
-end
-
+message = n > 10 ? '10より大きい' : '10以下'
 puts message
